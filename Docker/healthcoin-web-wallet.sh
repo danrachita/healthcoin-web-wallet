62849eb790135932c69c60dd82821f7303184533
onsightit <steve@onsightit.com>
2016-09-25 17:23:48 -0600

    add Docker support

---
+++ Docker/healthcoin-web-wallet.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+trap "exit 255" SIGINT SIGTERM
+
+while :
+do
+    DATETIME=`date`
+    /usr/bin/daemon --running --name healthcoin-web-wallet
+    if [ $? == 1 ] ; then
+        echo "*** $DATETIME : Starting healthcoin-web-wallet..."
+        cd ${HOME}
+        /bin/mv healthcoin-web-wallet.log.gz healthcoin-web-wallet.log.gz.bak
+        /bin/gzip healthcoin-web-wallet.log
+        /usr/bin/daemon --name "healthcoin-web-wallet" -D ${HOME}/healthcoin-web-wallet -o ${HOME}/healthcoin-web-wallet.log -- /usr/bin/nodejs --stack-size=10000 ${HOME}/healthcoin-web-wallet/app.js
+    fi
+    sleep 60
+done
