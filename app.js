586d1950044383ef547bfff63e286fc2fc2a9b87
onsightit <steve@onsightit.com>
2016-10-06 15:33:01 -0600

    flash error

--- app.js
+++ app.js
@@ -86,252 +86,249 @@ function healthcoinHandler(err, result){
     };
     this.res.send(JSON.stringify(response));
 }
-// healthcoinappObj exported for other modules.
-var healthcoinappObj = {};
+
+var healthcoinappObj = {}; // healthcoinappObj exported for other modules.
 healthcoinappObj.callHealthcoin = callHealthcoin;
 healthcoinappObj.healthcoinHandler = healthcoinHandler;
 module.exports = healthcoinappObj;
 
-//------- app.js CODE GOES HERE -------
-function healthcoinapp() {
+// Auth begin
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
 
-    // Auth begin
-    var cookieParser = require('cookie-parser');
-    var session = require('express-session');
-    var morgan = require('morgan');
-    var mongoose = require('mongoose');
-    var passport = require('passport');
-    var flash = require('connect-flash');
+var configDB = require('./healthcoin/database.js');
+mongoose.connect(configDB.url);
+require('./healthcoin/passport')(passport); // Requires healthcoinappObj export first.
 
-    var configDB = require('./healthcoin/database.js');
-    mongoose.connect(configDB.url);
-    require('./healthcoin/passport')(passport); // Requires healthcoinappObj export.
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({secret: 'nequals1',
+                 saveUninitialized: true,
+                 resave: true}));
 
-    app.use(morgan('dev'));
-    app.use(cookieParser());
-    app.use(bodyParser.urlencoded({extended: false}));
-    app.use(session({secret: 'nequals1',
-                     saveUninitialized: true,
-                     resave: true}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-    app.use(passport.initialize());
-    app.use(passport.session()); // persistent login sessions
-    app.use(flash()); // use connect-flash for flash messages stored in session
+require('./routes/auth.js')(app, passport); // Auth routes (includes: '/', '/signup', '/login', '/logout', '/profile', + oauth routes).
+// Auth end
 
-    require('./routes/auth.js')(app, passport); // Auth routes (includes: '/', '/signup', '/login', '/logout', '/profile', + oauth routes).
-    // Auth end
+// Non-RPC routes //
 
+// Not an RPC call, but returns true if the RPC node is localhost.
+app.get('/islocal', function(req,res){
+    var response = {
+        error: null,
+        result: isLocal
+    };
+    res.send(JSON.stringify(response));
+});
+
+// RPC routes //
+
+app.get('/getinfo', function(req,res){ callHealthcoin('getInfo', res, healthcoinHandler); } );
+app.get('/getinterestrate', function(req,res){ callHealthcoin('getInterestRate', res, healthcoinHandler); } );
+app.get('/getinflationrate', function(req,res){ callHealthcoin('getInflationRate', res, healthcoinHandler); } );
+app.get('/getblockcount', function(req,res){ callHealthcoin('getBlockCount', res, healthcoinHandler); } );
+app.get('/getstakinginfo', function(req,res) { callHealthcoin('getStakingInfo', res, healthcoinHandler); } );
+
+// pagination view
+app.get('/listtransactions/:account?/:page?', function(req, res){
+    var account = (req.params.account || '*'),
+        page = (req.params.page || 1),
+        count = 10,     // TODO: Parameterize this.
+        from = 0;
+    if (page < 1) page = 1;
+    from = count * page - count;
+    callHealthcoin('listTransactions', res, healthcoinHandler, account, count, from);
+});
+
+// Force new addresses to have an account
+app.get('/getnewaddress/:account', function(req, res){
+    var accountName = req.params.account || '';
+    if(accountName === "")
+        res.send(JSON.stringify("Error: Account cannot be blank."));
+    else
+        callHealthcoin('getnewaddress', res, healthcoinHandler, accountName);
+});
+
+app.get('/sendtoaddress/:toaddress/:amount', function(req, res){
+    var amount = parseFloat(req.params.amount);
+    callHealthcoin('sendtoaddress', res, healthcoinHandler, req.params.toaddress, amount);
+});
+
+app.get('/encryptwallet/:passphrase', function(req,res){
+    callHealthcoin('encryptwallet', res, healthcoinHandler, req.params.passphrase);
+});
+
+app.get('/walletpassphrase/:passphrase?/:timeout?/:stakingonly?', function(req,res){
+    var stakingOnly = req.params.stakingonly === 'true',
+        timeout = parseInt(req.params.timeout),
+        passphrase = decodeURIComponent(req.params.passphrase);
+    if (passphrase){
+        callHealthcoin('walletpassphrase', res, healthcoinHandler, req.params.passphrase, timeout, stakingOnly);
+    }
+});
+
+app.get('/walletlock', function(req,res){ callHealthcoin('walletlock', res, healthcoinHandler); });
+
+app.get('/help/:commandname?', function(req, res){
+    if (req.params.commandname !== undefined)
+        callHealthcoin('help', res, healthcoinHandler, req.params.commandname);
+    else
+        callHealthcoin('help', res, healthcoinHandler);
+});
+
+app.get('/listreceivedbyaddress/:minconf?/:includeempty?', function(req, res){
+    var includeEmpty = (req.params.includeempty || false) === 'true', 
+        minConf = parseInt(req.params.minconf || 1);
+    callHealthcoin('listreceivedbyaddress', res, healthcoinHandler, minConf, includeEmpty);
+});
+
+app.get('/getaccount/:address', function(req, res){
+    healthcoin.getaccount(req.params.address, function(err, result){
+        console.log("err:"+err+" result:"+result);
+        if(err)
+            res.send(err);
+        else
+            res.send(JSON.stringify(result));
+    });
+});
+
+app.get('/getbalance', function(req, res){
+    healthcoin.getbalance(function(err, result){
+        console.log("err:"+err+" result:"+result);
+        if(err)
+            res.send(err);
+        else
+            res.send(JSON.stringify(result));
+    });
+});
+
+app.get('/listaddressgroupings', function(req, res){
+    healthcoin.listaddressgroupings(function(err, result){
+        console.log("err:"+err+" result:"+result);
+        if(err)
+            res.send(err);
+        else
+            res.send(JSON.stringify(result));
+    });
+});
+
+app.get('/sendfrom/:fromaccount/:toaddress/:amount', function(req, res){
+    healthcoin.sendfrom(req.params.fromaccount, req.params.toaddress, parseInt(req.params.amount), function(err, result){
+        console.log("err:"+err+" result:"+result);
+        if(err)
+            res.send(err);
+        else
+            res.send(JSON.stringify(result));
+    });
+});
+
+app.get('/setaccount/:address/:account', function(req, res){
+    healthcoin.setaccount(req.params.address, req.params.account, function(err, result){
+        console.log("err:"+err+" result:"+result);
+        if(err)
+            res.send(err);
+        else
+            res.send(JSON.stringify(result));
+    });
+});
+
+app.get('/setadressbookname/:address/:label', function(req, res){
+    healthcoin.setadressbookname(req.params.address, req.params.label, function(err, result){
+        console.log("err:"+err+" result:"+result);
+        if(err)
+            res.send(err);
+        else
+            res.send(JSON.stringify(result));
+    });
+});
+
+// Custom routes //
+
+app.get('/totalhealthcoin', function(req, res){
+    healthcoin.getinfo(function(err, result){
+        console.log("err:"+err+" result:"+result);
+        if(err)
+            res.send(err);
+        else{
+            var money = result.moneysupply.toString();	
+            res.send(money);
+        }
+    });
+});
+
+app.get('/blockcount', function(req,res){
+    healthcoin.getinfo(function(err, result){
+        console.log("err:"+err+" result:"+result);
+        if(err)
+            res.send(err);
+        else{
+            var blocks = result.blocks.toString();
+            res.send(blocks);
+        }
+    });
+});
+
+app.get('/difficulty', function(req,res){
+    healthcoin.getDifficulty(function(err, result){
+        console.log("err:"+err+" result:"+result);
+        if(err)
+            res.send(err);
+        else
+            res.send(result);
+    });
+});	
+
+app.get('/getblockhash/:index', function(req, res){
+    healthcoin.getblockhash(parseInt(req.params.index), function(err, hash){
+        if(err)
+            res.send(err);
+        else
+            res.send(hash);
+    });
+});
+
+app.get('/getblock/:hash', function(req, res){
+    healthcoin.getblock(req.params.hash, function(err, data){
+        if(err)
+            res.send(err);
+        else
+            res.render('block', data);
+    });
+});
+
+app.get('/gettx/:txid', function(req, res){
+    healthcoin.gettransaction(req.params.txid, function(err, data){
+        if(err)
+            res.send("Error parsing transaction id");
+        else
+            res.render('tx', data);
+    });
+});
+
+app.get('/getpeers', function(req, res){
+    healthcoin.getpeers(function(err, result){
+        if(err)
+            res.send(err);
+        else
+            res.send(JSON.stringify(result));
+    });
+});
+
+// Start it up!
+function healthcoinapp(app) {
     // Start the Healthcoin Express server
     console.log('Healthcoin Express server starting');
     var server = http.createServer(app).listen(app.get('port'), function(){
         console.log('Express server listening on port ' + app.get('port'));
     });
-
-    // Non-RPC routes
-
-    // Not an RPC call, but returns true if the RPC node is localhost.
-    app.get('/islocal', function(req,res){
-        var response = {
-            error: null,
-            result: isLocal
-        };
-        res.send(JSON.stringify(response));
-    });
-
-    // RPC routes
-
-    app.get('/getinfo', function(req,res){ callHealthcoin('getInfo', res, healthcoinHandler); } );
-    app.get('/getinterestrate', function(req,res){ callHealthcoin('getInterestRate', res, healthcoinHandler); } );
-    app.get('/getinflationrate', function(req,res){ callHealthcoin('getInflationRate', res, healthcoinHandler); } );
-    app.get('/getblockcount', function(req,res){ callHealthcoin('getBlockCount', res, healthcoinHandler); } );
-    app.get('/getstakinginfo', function(req,res) { callHealthcoin('getStakingInfo', res, healthcoinHandler); } );
-
-    // pagination view
-    app.get('/listtransactions/:account?/:page?', function(req, res){
-        var account = (req.params.account || '*'),
-            page = (req.params.page || 1),
-            count = 10,     // TODO: Parameterize this.
-            from = 0;
-        if (page < 1) page = 1;
-        from = count * page - count;
-        callHealthcoin('listTransactions', res, healthcoinHandler, account, count, from);
-    });
-
-    // Force new addresses to have an account
-    app.get('/getnewaddress/:account', function(req, res){
-        var accountName = req.params.account || '';
-        if(accountName === "")
-            res.send(JSON.stringify("Error: Account cannot be blank."));
-        else
-            callHealthcoin('getnewaddress', res, healthcoinHandler, accountName);
-    });
-
-    app.get('/sendtoaddress/:toaddress/:amount', function(req, res){
-        var amount = parseFloat(req.params.amount);
-        callHealthcoin('sendtoaddress', res, healthcoinHandler, req.params.toaddress, amount);
-    });
-
-    app.get('/encryptwallet/:passphrase', function(req,res){
-        callHealthcoin('encryptwallet', res, healthcoinHandler, req.params.passphrase);
-    });
-
-    app.get('/walletpassphrase/:passphrase?/:timeout?/:stakingonly?', function(req,res){
-        var stakingOnly = req.params.stakingonly === 'true',
-            timeout = parseInt(req.params.timeout),
-            passphrase = decodeURIComponent(req.params.passphrase);
-        if (passphrase){
-            callHealthcoin('walletpassphrase', res, healthcoinHandler, req.params.passphrase, timeout, stakingOnly);
-        }
-    });
-
-    app.get('/walletlock', function(req,res){ callHealthcoin('walletlock', res, healthcoinHandler); });
-
-    app.get('/help/:commandname?', function(req, res){
-        if (req.params.commandname !== undefined)
-            callHealthcoin('help', res, healthcoinHandler, req.params.commandname);
-        else
-            callHealthcoin('help', res, healthcoinHandler);
-    });
-
-    app.get('/listreceivedbyaddress/:minconf?/:includeempty?', function(req, res){
-        var includeEmpty = (req.params.includeempty || false) === 'true', 
-            minConf = parseInt(req.params.minconf || 1);
-        callHealthcoin('listreceivedbyaddress', res, healthcoinHandler, minConf, includeEmpty);
-    });
-
-    app.get('/getaccount/:address', function(req, res){
-        healthcoin.getaccount(req.params.address, function(err, result){
-            console.log("err:"+err+" result:"+result);
-            if(err)
-                res.send(err);
-            else
-                res.send(JSON.stringify(result));
-        });
-    });
-
-    app.get('/getbalance', function(req, res){
-        healthcoin.getbalance(function(err, result){
-            console.log("err:"+err+" result:"+result);
-            if(err)
-                res.send(err);
-            else
-                res.send(JSON.stringify(result));
-        });
-    });
-
-    app.get('/listaddressgroupings', function(req, res){
-        healthcoin.listaddressgroupings(function(err, result){
-            console.log("err:"+err+" result:"+result);
-            if(err)
-                res.send(err);
-            else
-                res.send(JSON.stringify(result));
-        });
-    });
-
-    app.get('/sendfrom/:fromaccount/:toaddress/:amount', function(req, res){
-        healthcoin.sendfrom(req.params.fromaccount, req.params.toaddress, parseInt(req.params.amount), function(err, result){
-            console.log("err:"+err+" result:"+result);
-            if(err)
-                res.send(err);
-            else
-                res.send(JSON.stringify(result));
-        });
-    });
-
-    app.get('/setaccount/:address/:account', function(req, res){
-        healthcoin.setaccount(req.params.address, req.params.account, function(err, result){
-            console.log("err:"+err+" result:"+result);
-            if(err)
-                res.send(err);
-            else
-                res.send(JSON.stringify(result));
-        });
-    });
-
-    app.get('/setadressbookname/:address/:label', function(req, res){
-        healthcoin.setadressbookname(req.params.address, req.params.label, function(err, result){
-            console.log("err:"+err+" result:"+result);
-            if(err)
-                res.send(err);
-            else
-                res.send(JSON.stringify(result));
-        });
-    });
-
-    // Custom routes
-
-    app.get('/totalhealthcoin', function(req, res){
-        healthcoin.getinfo(function(err, result){
-            console.log("err:"+err+" result:"+result);
-            if(err)
-                res.send(err);
-            else{
-                var money = result.moneysupply.toString();	
-                res.send(money);
-            }
-        });
-    });
-
-    app.get('/blockcount', function(req,res){
-        healthcoin.getinfo(function(err, result){
-            console.log("err:"+err+" result:"+result);
-            if(err)
-                res.send(err);
-            else{
-                var blocks = result.blocks.toString();
-                res.send(blocks);
-            }
-        });
-    });
-
-    app.get('/difficulty', function(req,res){
-        healthcoin.getDifficulty(function(err, result){
-            console.log("err:"+err+" result:"+result);
-            if(err)
-                res.send(err);
-            else
-                res.send(result);
-        });
-    });	
-
-    app.get('/getblockhash/:index', function(req, res){
-        healthcoin.getblockhash(parseInt(req.params.index), function(err, hash){
-            if(err)
-                res.send(err);
-            else
-                res.send(hash);
-        });
-    });
-
-    app.get('/getblock/:hash', function(req, res){
-        healthcoin.getblock(req.params.hash, function(err, data){
-            if(err)
-                res.send(err);
-            else
-                res.render('block', data);
-        });
-    });
-
-    app.get('/gettx/:txid', function(req, res){
-        healthcoin.gettransaction(req.params.txid, function(err, data){
-            if(err)
-                res.send("Error parsing transaction id");
-            else
-                res.render('tx', data);
-        });
-    });
-
-    app.get('/getpeers', function(req, res){
-        healthcoin.getpeers(function(err, result){
-            if(err)
-                res.send(err);
-            else
-                res.send(JSON.stringify(result));
-        });
-    });
-
 }
-//------- app.js CODE ENDS -------
 
-healthcoinapp();
+healthcoinapp(app);
