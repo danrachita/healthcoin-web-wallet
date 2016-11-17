d291b73274bbbb0b08b21740ac690363b4ba5711
onsightit <steve@onsightit.com>
2016-11-16 18:35:22 -0700

    user session tweaks

--- app.js
+++ app.js
@@ -99,8 +99,9 @@ var Biomarkers = require('./healthcoin/biomarkers');
 // Add CORS headers to all requests
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
