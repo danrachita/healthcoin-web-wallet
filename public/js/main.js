873579b7b2a2be10045b274fb9d71c511aa5a4d3
onsightit <steve@onsightit.com>
2016-11-20 15:43:30 -0700

    reordering

--- public/js/main.js
+++ public/js/main.js
@@ -4,10 +4,10 @@ require.config({
         bootstrap: 'lib/bootstrap.min',
         "bootstrap-editable": 'lib/bootstrap-editable-customized.min',
         knockout: 'lib/knockout',
+        sammy: "lib/sammy",
         "knockout-amd-helpers": 'lib/knockout-amd-helpers',
         "knockout-validation": 'lib/knockout.validation.min',
         "knockout-x-editable": 'lib/knockout.x-editable.min',
-        sammy: "lib/sammy",
         text: "lib/text",
         patterns: 'extenders/patterns'
     }
