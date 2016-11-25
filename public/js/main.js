59072a235e178cdb0fcb0f8121a47a753a2178f1
onsightit <steve@onsightit.com>
2016-11-25 07:43:06 -0700

    force require to prevent undefined

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,11 @@ require.config({
     paths: {
         jquery: 'lib/jquery.min', // 2.2.4
         sammy: "lib/sammy",
-        moment: "lib/moment.min",
+        //moment: "lib/moment.min",
+        moment: "lib/moment",
         bootstrap: 'lib/bootstrap.min',
-        "bootstrap-editable": 'lib/bootstrap-editable-customized.min',
+        //"bootstrap-editable": 'lib/bootstrap-editable-customized.min',
+        "bootstrap-editable": 'lib/bootstrap-editable',
         knockout: 'lib/knockout',
         "knockout-amd-helpers": 'lib/knockout-amd-helpers',
         "knockout-validation": 'lib/knockout.validation.min',
