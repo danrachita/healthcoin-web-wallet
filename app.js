91d4cf161abe4a1661aff3eaa8d0619de857678a
onsightit <steve@onsightit.com>
2017-01-28 09:43:00 -0700

    db error handling

--- app.js
+++ app.js
@@ -555,6 +555,13 @@ function startApp(app) {
             io.sockets.emit('news', 'Down for database maintenance...');
             io.sockets.emit('abort', 'maintenance');
         });
+        process.on('database_error', function (err) {
+            console.log(err);
+            app.set('status', coin.settings.coinName + ' Maintenance Underway');
+            // Send abort message to all clients
+            io.sockets.emit('news', 'Database error...');
+            io.sockets.emit('abort', 'maintenance');
+        });
         process.on('database_reconnected', function (err) {
             console.log(err);
             app.set('status', '');
@@ -568,13 +575,6 @@ function startApp(app) {
             // Send abort message to all clients
             io.sockets.emit('news', 'Database is connected...');
             io.sockets.emit('continue', '');
-        });
-        process.on('database_error', function (err) {
-            console.log(err);
-            app.set('status', '');
-            // Send abort message to all clients
-            io.sockets.emit('news', 'Database error...');
-            io.sockets.emit('abort', 'maintenance');
         });
         process.on('SIGINT', function(err){
             console.log('SIGINT Received: ' + err);
